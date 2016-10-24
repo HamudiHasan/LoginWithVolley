e513cf56cac6f230b3a23e633db87553050315fd
HamudiHasan <hhsonet@gmail.com>
2016-10-24 20:14:17 +0600

    Initial commit

---
+++ app/src/main/java/com/tesseractbd/loginwithvolley/MainActivity.java
@@ -0,0 +1,81 @@
+package com.tesseractbd.loginwithvolley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUserName;
+    EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUserName = (EditText) findViewById(R.id.et_user_name);
+        etPassword = (EditText) findViewById(R.id.et_password);
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isNotEmpty(etUserName )&& isNotEmpty(etPassword)){
+                    loginRequest(etUserName.getText().toString(),etPassword.getText().toString());
+                }
+            }
+        });
+    }
+    private boolean isNotEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return true;
+
+        return false;
+    }
+
+    public void loginRequest(final String userName, final String password){
+
+        String url = "http://rabbitshat.com/apto/api/UserAuthorizationsAPI?aid="+userName+"&"+"code="+password;
+        StringRequest sr = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.equals("An error has occurred.")){
+                            Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Successfully login as "+response, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue rq = Volley.newRequestQueue(MainActivity.this);
+        rq.add(sr);
+
+    }
+}
